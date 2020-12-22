5f4a357e397194bf0495ba02673467e0ca31597f
Zain Mir <zainnaeemk10@gmail.com>
2020-12-22 19:46:31 +0500

    wargae bhai

--- admin.php
+++ admin.php
@@ -1,3 +1,38 @@
+<?php
+
+$servername = "localhost";
+ $username = "username";
+ $Email ="Email";
+ $password = "password";
+ $DB= "webroject";
+ $conn = new mysqli("localhost","root","",$DB);
+if (!$conn) {
+     die("Connection failed: " . $con->connect_error);
+}
+
+
+if(isset($_REQUEST['submit']))
+{  
+  if(($_REQUEST['username'] == "")||($_REQUEST['Email'] == "")||($_REQUEST['Password']== "")){
+          echo "Fill all the fields";
+         }
+         else{
+          $name= $_POST['username'];
+          $Email = $_POST['Email'];
+          $password =$_POST['Password'];
+          $sql = "INSERT INTO `products`(`ProductID`, `Name`, `Description`, `Brand`, `RAM`, `ROM`, `images`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7])";
+          //inserting into database
+          if ($conn->query($sql) === TRUE) {
+           
+            header("Location: http://localhost/Project 1/index.php");
+            exit;
+                     } else {
+                       echo "Error Entering database: " . $conn->error;
+                      }
+                   
+}
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -22,24 +57,24 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-6">
-                        <label for="name">Mobile Name: &nbsp</label><input type="text" name="name">
+                        <label for="name">Mobile Name: &nbsp</label><input type="text" name="mobilename">
                     </div>
                     <div class="col-lg-6">
-                        <label for="name">RAM: &nbsp</label><input type="text" name="name">
+                        <label for="name">RAM: &nbsp</label><input type="int" name="RAM">
                     </div>
                     <div class="col-lg-6">
-                        <label for="name">Description: &nbsp&nbsp&nbsp</label><input type="text" name="name">
+                        <label for="name">Description: &nbsp&nbsp&nbsp</label><input type="text" name="description">
                     </div>
                     <div class="col-lg-6">
-                        <label for="name">ROM: &nbsp</label><input type="text" name="name">
+                        <label for="name">ROM: &nbsp</label><input type="text" name="ROM">
                     </div>
                     <div class="col-lg-6">
                         <label for="mob-type">
                             Mobile-Type: &nbsp&nbsp
                         </label>
                         <select name="mob-type" id="">
-                            <option value="samsung">Samsung</option>
-                            <option value="iphone">Iphone</option>
+                            <option name="samsung" value="samsung">Samsung</option>
+                            <option name="iphone"value="iphone">Iphone</option>
                         </select>
                     </div>
                     <div class="col-lg-12">
